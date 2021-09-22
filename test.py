220f113d5d60504646a15db86e4a80d88bb3813b
Tirasz <zacsi143@gmail.com>
2021-09-22 22:33:19 +0200

    Test file added

---
+++ test.py
@@ -0,0 +1,14 @@
+numbi = 3
+if numbi == 0:
+    print("Nothing")
+elif numbi == 1:
+    print("Alone and afraid")
+elif numbi == 2:
+    print(".. and make it double!")
+else:
+    print(f"The answer to life, the universe, and everything: {numbi}!")
+
+
+
+if numbi > 0:
+    print("awsd")
