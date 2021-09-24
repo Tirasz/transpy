8566171beb727069c5f402f1fc24709f62b97269
Tirasz <zacsi143@gmail.com>
2021-09-24 14:41:13 +0200

    ...

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 numbi = 3
+asd = 4
+
 if numbi == 0:
     print("Nothing")
 elif numbi == 1:
@@ -7,6 +9,17 @@ elif numbi == 2:
     print(".. and make it double!")
 else:
     print(f"The answer to life, the universe, and everything: {numbi}!")
+
+if asd == 0:
+    print("Nothing")
+elif 1 == asd:
+    print("Alone and afraid")
+elif asd == 2:
+    print(".. and make it double!")
+elif asd > 2:
+    print("asdasdasdsda")
+    
+
 
 
 
