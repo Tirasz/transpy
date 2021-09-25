8386b53c7874fbbe5e4696e9e366dc888c9e00c1
Tirasz <zacsi143@gmail.com>
2021-09-25 17:27:25 +0200

    something something refactor

--- analyzer.py
+++ analyzer.py
@@ -1,9 +1,11 @@
 import ast
 
+MIN_BRANCHES = 4
 
 def get_var_const_from_if(node):
+    #Returns a tuple of (id,const) 
     #Returns false if the If-node's test is not either in the form 'id == constant' or 'constant == id'
-    #Returns a tuple of (id,const)
+
     match node.test:
         case ast.Compare(left=ast.Name(id=var_id, ctx = _), ops =[ast.Eq()], comparators=[ast.Constant(const)]) | ast.Compare(left=ast.Constant(const), ops =[ast.Eq()], comparators=[ast.Name(id=var_id, ctx = _)]):
             return (var_id, const)
@@ -25,19 +27,17 @@ class Analyzer(ast.NodeVisitor):
  
 
     def visit_If(self, node):
-        #print(f"## Found and If, at line number: {node.lineno} ##")
-        variable_id = ""
-        last_line = -1
-
+        print(f"## Found and If, at line number: {node.lineno} ##")
         variable_id = get_var_const_from_if(node)[0]
+
         if not variable_id:
             return
 
-        depth = 0
+        branches = 1
         current = node 
         # Going through and counting all the elif nodes and checking their form
         while len(current.orelse) and isinstance(current.orelse[0], ast.If):
-            depth += 1
+            branches += 1
             current = current.orelse[0]
             if not get_var_const_from_if(current)[0] == variable_id:
                 return
@@ -46,18 +46,14 @@ class Analyzer(ast.NodeVisitor):
         if not len(current.orelse): # If there is no last else: block
             last_line = current.body[-1].lineno
         else:
+            branches += 1
             last_line = current.orelse[-1].lineno
 
-        #print(f"# This If that spans from line number ({node.lineno} - {last_line}) has ({depth}) elif-s #")
-        self.save_code(node, node.lineno, last_line)
+        if branches >= MIN_BRANCHES:
+            print(f"# This If that spans from line number ({node.lineno} - {last_line}) has ({branches}) branches and has correct form. #")
+            self.save_code(node, node.lineno, last_line)
 
-
-   
-
-
-
-
-
+        
 
 def main():
     with open("test.py", "r") as src:
@@ -66,5 +62,6 @@ def main():
 
 if __name__ == "__main__":
     main()
+    print("Done")
 
     
