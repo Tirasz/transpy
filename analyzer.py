7f57be863bf586e0177a5602becdd3fc072178d4
Tirasz <zacsi143@gmail.com>
2021-09-26 15:33:07 +0200

    10th refactor party.

--- analyzer.py
+++ analyzer.py
@@ -2,15 +2,45 @@ import ast
 
 MIN_BRANCHES = 4
 
-def get_var_const_from_if(node):
+
+def get_var_const(node): 
     #Returns a tuple of (id,const) 
     #Returns false if the If-node's test is not either in the form 'id == constant' or 'constant == id'
 
-    match node.test:
+    if(isinstance(node, ast.If)):
+        test = node.test
+    elif(isinstance(node, ast.Compare)):
+        test = node
+    else:
+        raise ValueError(f"Cannot check the 'test' of type: ({node.__class__.__name__})!")
+
+    match test:
         case ast.Compare(left=ast.Name(id=var_id, ctx = _), ops =[ast.Eq()], comparators=[ast.Constant(const)]) | ast.Compare(left=ast.Constant(const), ops =[ast.Eq()], comparators=[ast.Name(id=var_id, ctx = _)]):
             return (var_id, const)
         case _:
             return False
+
+
+def get_branches(node):
+    # Returns a list of (test, body) tuples for each branch of the given 'If' node
+    # On 'else:' blocks with no 'test', returns (0, [nodes])
+
+    if not isinstance(node, ast.If):
+        raise ValueError(f"Cannot get branches for type: ({node.__class__.__name__})!")
+    branches = []
+    current = node
+
+    while(True):
+        branches.append((current.test, current.body))
+        #The 'orelse' of an 'If' node can be: 
+        match current.orelse:
+            case []: # an empty list, if there are no more branches
+                return branches
+            case [ast.If(test=_, body=_, orelse=_)]: # an 'If' node if there is another branch
+                current = current.orelse[0]
+            case [*nodes]: # can be a list of nodes, if its the last 'else:' block
+                branches.append((0, nodes))
+                return branches
 
 
 class Analyzer(ast.NodeVisitor):
@@ -24,33 +54,28 @@ class Analyzer(ast.NodeVisitor):
     def save_code(self, node, start, end):
         self.save_file.write(f"# {start} - {end}\n" + ast.unparse(node) + "\n")
 
- 
+
 
     def visit_If(self, node):
         print(f"## Found and If, at line number: {node.lineno} ##")
-        variable_id = get_var_const_from_if(node)[0]
-
-        if not variable_id:
+        # Checking if the main If node is in correct form
+        subject = get_var_const(node)[0]
+        if not subject:
             return
 
-        branches = 1
-        current = node 
-        # Going through and counting all the elif nodes and checking their form
-        while len(current.orelse) and isinstance(current.orelse[0], ast.If):
-            branches += 1
-            current = current.orelse[0]
-            if not get_var_const_from_if(current)[0] == variable_id:
+        # Checking if all the branches are in correct form
+        branches = get_branches(node)
+        for branch in branches:
+            test = branch[0]
+            if test != 0 and get_var_const(test)[0] != subject:
                 return
+                
+        # The last line is equal to the last node in the body of the last branch
+        # This might need to be refactored
+        last_line = branches[-1][1][-1].lineno        
 
-            
-        if not len(current.orelse): # If there is no last else: block
-            last_line = current.body[-1].lineno
-        else:
-            branches += 1
-            last_line = current.orelse[-1].lineno
-
-        if branches >= MIN_BRANCHES:
-            print(f"# This If that spans from line number ({node.lineno} - {last_line}) has ({branches}) branches and has correct form. #")
+        if len(branches) >= MIN_BRANCHES:
+            print(f"# This If that spans from line number ({node.lineno} - {last_line}) has ({len(branches)}) branches and has correct form. #")
             self.save_code(node, node.lineno, last_line)
 
         
