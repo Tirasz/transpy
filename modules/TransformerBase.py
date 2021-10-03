82005bef44b1e9c1cc8c3556f3cd630f1d9eb61c
Tirasz <zacsi143@gmail.com>
2021-10-03 21:07:57 +0200

    Added comments

--- modules/TransformerBase.py
+++ modules/TransformerBase.py
@@ -2,6 +2,12 @@ import abc
 import ast
 
 class TransformerBase(metaclass=abc.ABCMeta):
+    """
+    Defines an interface for a transformer module. 
+    Every transformer should implement a visit(node), and a transform(node) method.
+    visit(node): Analyzes the node, and determines if it can be transformed by the module. Returns a boolean.
+    transform(node): Returns the transformed node. 
+    """
     @classmethod
     def __subclasshook__(cls, subclass):
         return (hasattr(subclass, 'visit') and 
