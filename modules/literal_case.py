84a06d4f9f4be93607c09f2b03b70edae10feee4
Tirasz <zacsi143@gmail.com>
2021-10-03 21:46:45 +0200

    minor refactor

--- modules/literal_case.py
+++ modules/literal_case.py
@@ -5,13 +5,13 @@ def get_var_const(node):
     #Returns a tuple of (id,const) 
     #Returns false if the If-node's test is not either in the form 'id == constant' or 'constant == id'
 
-    if(isinstance(node, ast.If)):
-        test = node.test
-    elif(isinstance(node, ast.Compare)):
-        test = node
-    else:
+    if not isinstance(node, ast.If) or isinstance(node, ast.Compare):
         raise ValueError(f"Cannot check the 'test' of type: ({node.__class__.__name__})!")
 
+    test = node
+    if(isinstance(node, ast.If)):
+        test = node.test
+    
     match test:
         case ast.Compare(left=ast.Name(id=var_id, ctx = _), ops =[ast.Eq()], comparators=[ast.Constant(const)]) | ast.Compare(left=ast.Constant(const), ops =[ast.Eq()], comparators=[ast.Name(id=var_id, ctx = _)]):
             return (var_id, const)
@@ -26,11 +26,12 @@ class LiteralCase:
         if not subject:
             return False
 
-        # Checking if all the branches are in correct form
+        
         branches = get_branches(node)
         if len(branches) < MIN_BRANCHES:
             return False 
 
+        # Checking if all the branches are in correct form
         for branch in branches:
             curr_subject = get_var_const(branch.test)[0]
             if branch.test is not None and curr_subject != subject:
