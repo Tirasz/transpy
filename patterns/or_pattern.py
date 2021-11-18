56bb032da5ef3516b38430862d71f9994d104a50
Rózsa Balázs <h985122@stud.u-szeged.hu>
2021-11-18 20:57:03 +0100

    or-pattern subpattern recognition

--- patterns/or_pattern.py
+++ patterns/or_pattern.py
@@ -6,13 +6,27 @@ class OrPattern():
         self._potential_subjects = set()
 
     def visit(self, node):
-        match node:
-            case ast.BoolOp(op = ast.Or(), values = [*values]):
-                for value in values:
-                    self.terms.append(value)
-            case _:
-                return False
-        return True
+        # Or-pattern: T1 OR T2 [OR Tn]* 
+        # Where all Ti terms are valid patterns, and they share one possible subject.
+        if not ( isinstance(node, ast.BoolOp) and isinstance(node.op, ast.Or) ):
+            return False
+
+        for value in node.values:
+            for pattern in OrPattern.Patterns:
+                curr_pattern = pattern()
+                if curr_pattern.visit(value):
+                    self.terms.append(curr_pattern)
+                    print(f"PATTERN: ({ast.unparse(value)}) RECOGNISED BY: {type(curr_pattern).__name__}")
+                    break
+                
+        if len(node.values) != len(self.terms):
+            return False
+        # At this point, every term is a recognised pattern
+        self._potential_subjects = self.terms[0].potential_subjects().copy()
+        # Intersecting every terms potential subjects.
+        for term in self.terms:
+            self._potential_subjects = self._potential_subjects.intersection(term.potential_subjects())
+        return len(self._potential_subjects)
 
 
     def transform(self):
