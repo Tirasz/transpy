9bffb4a6332f003cbbdf044fa0a2c097d1ce5da3
Rózsa Balázs <h985122@stud.u-szeged.hu>
2021-11-18 21:55:09 +0100

    analyzer pattern iterating and transforming

--- analyzer.py
+++ analyzer.py
@@ -88,28 +88,49 @@ class Analyzer(ast.NodeVisitor):
     Patterns = None
 
     def __init__(self):
-        self.branches = {} # Mapping If-nodes to a list of its branches
+        self.branches = {} # Mapping If-nodes to a list of its branches. !!Only contains transformable if-nodes!!
         self.patterns = {} # Mapping branches to a pattern
+        self.subjects = {} # Mapping the If-node to its selected subject !!Only contains transformable if-nodes!!
         
     def visit_If(self, node):
         self.branches[node] = get_branches(node)
         for branch in self.branches[node]:
+                print(f"ANALYZER: BRANCH({branch.body[0].lineno-1})")
+                if branch.test is None:
+                    print(f"ANALYZER: TEST IS NONE. SKIPPING")
+                    continue
+
                 # Determine the main pattern of the branch
                 for pattern in Analyzer.Patterns:
                     curr_pattern = pattern()
                     if curr_pattern.visit(branch.test):
                         self.patterns[branch] = curr_pattern
+                        print(f"ANALYZER: ({ast.unparse(branch.test)}) RECOGNISED BY: {type(curr_pattern).__name__}")
                         break
 
                 # If no pattern recognises the branch, then delete the whole if node from the dict and return
                 if branch not in self.patterns.keys():
-                    print(f"NO PATTERN RECOGNISES BRANCH({branch.body[0].lineno -1})")
+                    print(f"ANALYZER: NO PATTERN RECOGNISES BRANCH({branch.body[0].lineno -1})")
                     del self.branches[node]
                     return
-                #print(f"BRANCH ({branch.body[0].lineno -1}): {self.patterns[branch]} ==> {self.patterns[branch].potential_subjects()}")
+                
+                print(f"ANALYZER: BRANCH({branch.body[0].lineno-1}) RECOGNISED BY: {type(self.patterns[branch]).__name__}")
 
-     
-
+        # An if-node can be transformed, if all of its branches are recognised patterns, and these patterns can all recognise the same subject.
+        # Intersecting the possible subjects for each branch
+        potential_subjects = self.patterns[self.branches[node][0]].potential_subjects().copy()
+        # Skipping "else:" branch
+        for branch in [b for b in self.branches[node] if b.test is not None]:
+            potential_subjects.intersection(self.patterns[branch].potential_subjects())
+ 
+        if len(potential_subjects) == 0:
+            print(f"ANALYZER: NO COMMON SUBJECT FOR IF-NODE AT: ({node.test.lineno})")
+            del self.branches[node]
+            return
+        elif len(potential_subjects) > 1:
+            print(f"ANALYZER: MORE THAN ONE COMMON SUBJECT FOR IF-NODE AT: ({node.test.lineno})")
+        self.subjects[node] = potential_subjects.pop()
+        print(f"ANALYZER: IF-NODE AT ({node.test.lineno}) HAS POTENTIAL SUBJECT: ({ast.unparse(self.subjects[node])})")
 
 
 
@@ -123,6 +144,22 @@ def main():
 
     analyzer = Analyzer()    
     analyzer.visit(tree)
+    with open("transformed.py", "w") as out:
+        for ifNode, subjectNode in analyzer.subjects.items():
+            _cases = []
+            out.write("#" + "-"*10 + str(ifNode.lineno) + "-"*10 + f"[{type(subjectNode).__name__}]" +"\n")
+            for branch in analyzer.branches[ifNode]:
+                if branch.test is not None:
+                    pattern = analyzer.patterns[branch]
+                    transformed_branch = ast.match_case(pattern = pattern.transform(subjectNode), guard = pattern.guard(subjectNode), body = branch.body)
+                else:
+                    transformed_branch = ast.match_case(pattern = ast.MatchAs(), guard = None, body = branch.body)
+
+                _cases.append(transformed_branch)
+
+            transformed_node = ast.Match(subject = subjectNode, cases = _cases)
+            out.write(ast.unparse(transformed_node) + "\n")
+
 
 
 if __name__ == "__main__":
