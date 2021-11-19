3fa47e0b46c59cd6f16baee83cbee8325b72f469
Rózsa Balázs <h985122@stud.u-szeged.hu>
2021-11-19 16:04:55 +0100

    bit more clarity maybe

--- patterns/Base.py
+++ patterns/Base.py
@@ -6,13 +6,13 @@ class PatternBase(metaclass=abc.ABCMeta):
     """
     Defines an interface for a transformer plugin. 
     Every transformer should implement:
-    visit(node): returns a boolean, true, if the node fits the pattern
-    transform(node): returns an ast pattern, that can be used inside an ast.match_case 
-    guard(subjectNode): Returns an expression, to put in guard. Can be different based on chosen subjectNode.
-    potential_subjects(): returns a set, containing nodes that the pattern recognises as a subject node. (subjects are used for ast.Match)
-    Every instance of a pattern should initialise a node attribute, that stores what node its visit method was called on originally.
+    - visit(node): returns a boolean, true, if the node fits the pattern
+    - transform(node): returns an ast pattern, that can be used inside an ast.match_case 
+    - guard(subjectNode): Returns an expression, to put in guard. Can be different based on chosen subjectNode.
+    - potential_subjects(): returns a set, containing nodes that the pattern recognises as a subject node. (subjects are used for ast.Match)
+    Every instance of a pattern should initialize a node attribute, that stores what node its visit method was called on originally.
     Every valid pattern class gets a static attribute 'Patterns', which is a tuple of all valid Pattern classes. This can be used to recognise sub-patterns.
-    Some patterns might require some back-tracking, to properly work. 
+    Some patterns might require some back-tracking, to properly work: 
     Every pattern should have a static boolean attribute, 'IsComplex' indicating this.
     If the pattern is complex, the pattern has to provide a method "process(parentPattern)", which the parent Pattern should call, passing itself.
     """
