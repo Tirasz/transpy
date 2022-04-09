ce2f1d1f89184999cfb254cf67c89214fe014b24
Tirasz <zacsi143@gmail.com>
2022-04-09 13:38:24 +0200

    spagetti testing

--- __main__.py
+++ __main__.py
@@ -28,7 +28,8 @@ TEST_DATA = {
     "no_nodes_visited" : 0,
     "no_nodes_transformed" : 0,
     "runtime_s" : 0,
-    "max_memory_MiB" : 0
+    "max_memory_MiB" : 0,
+    "max_workers": 0
 }
 
 def cloc(path):
@@ -55,14 +56,32 @@ def get_size(start_path):
     return total_size
 
 
-def test_helper(path):
+def test_helper_linear(path):
+    print("RUNNING LINEAR")
     global TEST_DATA
     files_to_transform = [f for f in path.rglob('*.py')] if path.is_dir() else [path]
-    init_output(path)
+    
 
     TEST_DATA["no_files"] = len(files_to_transform)
+    TEST_DATA["max_workers"] = max_threads
     print(f"Transforming files: ")
-    with concurrent.futures.ThreadPoolExecutor() as executor:
+    for file in files_to_transform:
+        visited, transformed = transform_helper(file)
+        TEST_DATA["no_nodes_visited"] += visited
+        TEST_DATA["no_nodes_transformed"] += transformed
+
+
+
+def test_helper(path, max_threads):
+    global TEST_DATA
+    files_to_transform = [f for f in path.rglob('*.py')] if path.is_dir() else [path]
+    
+
+    TEST_DATA["no_files"] = len(files_to_transform)
+    TEST_DATA["max_workers"] = max_threads
+    print(f"Transforming files: ")
+    
+    with concurrent.futures.ThreadPoolExecutor(max_workers=max_threads) as executor:
         results = list(tqdm(executor.map(transform_helper, files_to_transform), total=len(files_to_transform)))
 
     for res in results:
@@ -87,10 +106,32 @@ def make_copy(schizo, newPath):
     else:
         schizo[0] = shutil.copy(path,newPath)
 
-def main():
+def main(path, max_threads):
+    print("RUNNING IN TEST MODE")
+    global TEST_DATA
+    if max_threads == 0:
+        start_time = time.monotonic()
+        test_helper_linear(path)
+        end_time = time.monotonic()
+    else:
+        start_time = time.monotonic()
+            #mem = max(memory_usage((test_helper, (path,max_threads), {})))
+        test_helper(path, max_threads)
+        end_time = time.monotonic()
+    TEST_DATA["runtime_s"] = timedelta(seconds=end_time - start_time).total_seconds()
+        #TEST_DATA["max_memory_MiB"] = mem 
+    TEST_DATA["project_size_MiB"] = get_size(path) / 1048576 # get size returns bytes, 1 MiB = 2^20 bytes which is 1048576
+    TEST_DATA["cloc_py"] = cloc(path)
+        
+
+   
+
+if __name__ == "__main__":
     args = parser.parse_args()
     path = Path(args.path[0]).resolve()
-    max_threads = args.max_threads[0] if args.max_threads is not None else None
+    max_threads = int(args.max_threads[0]) if args.max_threads is not None else min(32, os.cpu_count() + 4)
+    # Changed in version 3.8: Default value of max_workers is changed to min(32, os.cpu_count() + 4). 
+    # https://docs.python.org/3/library/concurrent.futures.html
     ow = args.ow
     test_mode = args.test
     files_to_transform = []
@@ -99,11 +140,14 @@ def main():
     if not path.exists():
         parser.error("Given path does not exist!")
 
-    if max_threads is not None and max_threads <= 0:
+    if max_threads is not None and max_threads < 0:
         parser.error(f"You want to use {max_threads} threads, huh?")
 
     if args.mode == "copy":
-        newPath = (path.parent / f"transformed-{path.parts[-1]}")
+        if not test_mode:
+            newPath = (path.parent / f"transformed-{path.parts[-1]}")
+        else:
+            newPath = (path.parent / f"transformed-{path.parts[-1]}-{max_threads}-threads")
         if newPath.exists():
             while not (ow == "Y" or ow == "N"):
                 prompt = "Directory" if path.is_dir() else "File"
@@ -111,7 +155,7 @@ def main():
                 
             if ow == "N":
                 print("Quitting")
-                return
+                exit()
         
         path = [path]
         #make_copy(path, newPath)
@@ -126,29 +170,12 @@ def main():
         path = path[0]
         print('Done                 ')
 
+    init_output(path)
+    mem = max(memory_usage((main, (path, max_threads), {})))
+    TEST_DATA["max_memory_MiB"] = mem
+    print(f"Writing test data in: {path / f'TEST_DATA.json'}")
+    with open(path / f"TEST_DATA.json", "w") as f:
+        json.dump(TEST_DATA, f, indent=4)
 
-    if test_mode:
-        global TEST_DATA
-        start_time = time.monotonic()
-        mem = max(memory_usage((test_helper, (path,), {})))
-        end_time = time.monotonic()
-        TEST_DATA["runtime_s"] = timedelta(seconds=end_time - start_time).total_seconds()
-        TEST_DATA["max_memory_MiB"] = mem 
-        TEST_DATA["project_size_MiB"] = get_size(path) / 1048576 # get size returns bytes, 1 MiB = 2^20 bytes which is 1048576
-        TEST_DATA["cloc_py"] = cloc(path)
-        
-        print(f"Writing test data in: {path / f'TEST_DATA.json'}")
-        with open(path / f"TEST_DATA.json", "w") as f:
-            json.dump(TEST_DATA, f, indent=4)
-    else:
-        files_to_transform = [f for f in path.rglob('*.py')] if path.is_dir() else [path]
-        init_output(path)
-        print(f"Transforming files: ")
-        with concurrent.futures.ThreadPoolExecutor() as executor:
-            results = list(tqdm(executor.map(transform_helper, files_to_transform), total=len(files_to_transform)))
-   
-
-if __name__ == "__main__":
-    main()
 
 
